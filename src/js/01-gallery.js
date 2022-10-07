9714526f03f15ff2be8bc49a51ecb4fcef8e3334
maksymomelchuk <omelchukmaksym@gmail.com>
2022-10-07 11:52:42 +0300

    commit before sending to tutor

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,28 @@
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css'
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-
 // Create gallery
-const galleryRef = document.querySelector('.gallery')
+const galleryRef = document.querySelector('.gallery');
 
 const result = galleryItems
-  .map((item) => {
+  .map(item => {
     return `<a class="gallery__item" href="${item.original}" target="_blank">
   <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>`
+</a>`;
   })
-  .join('')
+  .join('');
 
 // Create HTML page
-galleryRef.insertAdjacentHTML('afterbegin', result)
+galleryRef.insertAdjacentHTML('afterbegin', result);
 
 // Show SimpleLightbox gallery
 const gallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
-})
-gallery.on('show.simplelightbox')
+});
+gallery.on('show.simplelightbox');
 
-gallery.on('error.simplelightbox')
+gallery.on('error.simplelightbox');
